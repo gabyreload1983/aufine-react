27a5bdd70d01953bab6283a4ce0690e2675881d6
Gabriel Godoy <gabyreload@gmail.com>
2023-03-10 18:13:29 -0300

    Company text was translated

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -9,43 +9,44 @@ function Company() {
           <h2>EMPRESA</h2>
           <p>
             Los principales productos de AUFINE incluyen neumáticos radiales
-            para camiones y autobuses, neumáticos para turismos, que han pasado
-            las certificaciones DOT, ECE, REACH, IS02001, S-MARK, CCC, GCC, BIS,
-            SMARTWAY, INMETRO, SASO, SONCAP, LATU, NOM. Neumático AUFINE La red
-            de distribución está en más de 130 países de Europa, América, Asia,
-            África y Oceanía.
+            para camiones y autobuses, neumáticos para turismos, que han
+            superado las certificaciones DOT, ECE, EACH, IS09001, S-MARK, CCC,
+            GCC, BIS, SMARTWAY, INMETRO, SASO, SONCAP, LATU, NOM. La red de
+            distribución de neumáticos de AUFINE se extiende a más de 130 países
+            de Europa, América, Asia, África y Oceanía.
           </p>
           <p>
-            Adherirse a la misión de brindar seguridad, comodidad y placer a los
-            conductores de todo el mundo, AUFINE se compromete con la innovación
-            y la actualización de productos como el corazón de empresa. La gente
-            de AUFINE está absorta en satisfacer las demandas individuales
-            locales. AUFINO tire toma la innovación como el poder de crecimiento
-            de la marca. Además, AUFINE tiene estándar Proceso de sistema de
-            desarrollo e investigación independiente y capacidad de producción
-            para desarrollar neumáticos de alto rendimiento con base ancha,
-            sección baja y bajo nivel de ruido.
+            Cumpliendo con la misión de brindar seguridad, comodidad y placer a
+            los conductores de todo el mundo, AUFINE se compromete con la
+            innovación y la mejora de los productos como el corazón de la
+            empresa. La gente de AUFINE está absorta en satisfacer las demandas
+            individuales locales. El neumático AUFINE toma la innovación como el
+            poder de crecimiento de la marca. Además, AUFINE tiene un proceso
+            estándar de sistema de desarrollo e investigación independiente y
+            capacidad de producción para desarrollar neumáticos de alto
+            rendimiento con base ancha, sección baja y bajo nivel de ruido.
           </p>
           <p>
             Dedicar todos los mejores servicios a los clientes es la actitud
-            invariable de AUFINE. AUFINO Los especialistas en ventas
-            profesionales le brindan una gama completa de servicios en
-            recomendaciones, pedidos, envíos y servicios post venta. Gerencia
-            regional de canales de mercado, alta coincidencia con productos y
-            canales e incentivo inspirador estrategia asegurar el crecimiento de
-            las ventas de los clientes. AUFINE ofrece artículos promocionales
-            diversificados a los clientes, especialmente con soportes
-            personalizados a los mercados locales. integrales y regionales La
-            estrategia de marketing lleva a AUFINE a un crecimiento más rápido.
+            invariable de AUFINE. Los especialistas en ventas profesionales de
+            AUFINE le brindan una gama completa de servicios sobre
+            recomendaciones, pedidos, envío y servicios posventa. La gestión
+            regional de los canales de mercado, la alta correspondencia con los
+            productos y canales y la estrategia de incentivos inspiradores
+            aseguran el crecimiento de las ventas de los clientes. AUFINE
+            proporciona artículos promocionales diversificados a los clientes,
+            especialmente con soportes personalizados para los mercados locales.
+            La estrategia de marketing integral y regional conduce a AUFINE para
+            un crecimiento más rápido.
           </p>
           <p>
             Entre la carretera y el vehículo, los conductores solo pueden
             confiar en los neumáticos, el neumático AUFINE es para escolta de
             seguridad. Entre los clientes y la marca, los mercados solo creen en
-            la alta calidad productos y servicios, AUFINE tire ofrece a los
+            productos y servicios de alta calidad, AUFINE tire brinda a los
             clientes confianza a largo plazo.
           </p>
-          <p>¡NEUMÁTICO AUFINO, PLACER ADELANTE INFINITO!</p>
+          <p>NEUMÁTICO AUFINO. ¡PLACER INFINITO HACIA ADELANTE!</p>
         </Col>
       </Row>
     </Container>
